b43e5648931190b43c99deaeebdf1f0e08439385
Lumpastyle <lumpastyle@gmail.com>
2016-05-13 15:38:02 +0200

    page controller & page repository, debut views

--- Controller/PageController.php
+++ Controller/PageController.php
@@ -8,49 +8,68 @@
  */
 
 namespace Controller;
-
 use Model\PageRepository;
 
 class PageController
 {
-    private $repository;
-
     /**
      * PageController constructor.
+     * @param \PDO $PDO
      */
-    public function __construct(\PDO $pdo)
+    public function __construct(\PDO $PDO)
     {
-        $this->repository = new PageRepository($pdo);
+        $this->repository = new PageRepository($PDO);
     }
-
+    /**
+     *
+     */
     public function ajoutAction()
     {
-
     }
-
+    /**
+     *
+     */
     public function supprimerAction()
     {
-
     }
-
+    /**
+     *
+     */
     public function modifierAction()
     {
-
     }
-
+    /**
+     *
+     */
     public function detailsAction()
     {
-
     }
-
+    /**
+     *
+     */
     public function listeAction()
     {
-
     }
-
+    /**
+     *
+     */
     public function displayAction()
     {
+        $slug = 'teletubbies';
+        if(isset($_GET['p'])){
+            $slug = $_GET['p'];
+        }
 
+        // en PHP 7
+        // $slug = $_GET['p'] ?? $_POST['p'] ?? 'teletubbies';
+
+        $page = $this->repository->getBySlug($slug);
+
+        if(!$page){
+            // 404
+            // include "View/404.php";
+            return;
+        }
+        include "View/page-display.php";
     }
-
 }
