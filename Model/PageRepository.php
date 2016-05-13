801a1715f9aeb6b4eb7efc77da492f7653245e98
Lumpastyle <lumpastyle@gmail.com>
2016-05-13 15:39:20 +0200

    page controller & page repository, debut views (#13)

--- Model/PageRepository.php
+++ Model/PageRepository.php
@@ -11,5 +11,69 @@ namespace Model;
 
 class PageRepository
 {
-
+    /**
+     * @var \PDO
+     */
+    private $PDO;
+    /**
+     * PageRepository constructor.
+     * @param \PDO $PDO
+     */
+    public function __construct(\PDO $PDO)
+    {
+        $this->PDO = $PDO;
+    }
+    /**
+     * @param null $id
+     * @return array
+     */
+    public function lister($id = null)
+    {
+        return [];
+    }
+    /**
+     * @param array $data
+     * @return bool
+     */
+    public function modifier(array $data)
+    {
+        return true;
+    }
+    /**
+     * @param int $id
+     * @return bool
+     */
+    public function supprimer(int $id)
+    {
+        return true;
+    }
+    /**
+     * @param array $data
+     * @return int
+     */
+    public function inserer(array $data)
+    {
+        return 1;
+    }
+    /**
+     * @param $slug
+     * @return \stdClass\bool
+     */
+    public function getBySlug($slug)
+    {
+        $sql ="SELECT
+                    `id`,
+                    `slug`,
+                    `body`,
+                    `title`
+                FROM
+                    `page`
+                WHERE
+                    `slug` = :slug
+                ";
+        $stmt = $this->PDO->prepare($sql);
+        $stmt->bindParam(':slug',$slug,\PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetchObject();
+    }
 }
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require_once "vendor/autoload.php";
+
+try{
+    $pdo = new \PDO("mysql:host=localhost;dbname=telekittyobj","root","root");
+    $pdo->query("SET NAMES 'UTF8';");
+}catch(PDOException $e){
+    die($e->getMessage());
+}
+
+$page = new \Controller\PageController($pdo);
+$page->displayAction();
