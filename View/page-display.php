8908c44923c5b6b05df497690182773295355aff
Lumpastyle <lumpastyle@gmail.com>
2016-05-24 10:02:51 +0200

    Commentaires et debut nav

--- View/page-display.php
+++ View/page-display.php
@@ -2,11 +2,15 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title><?php $titre ?></title>
+    <title><?=$page->title ?></title>
     <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
     <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
 </head>
 <body role="document">
+<?=$nav ?>
+<div class="container theme-showcase" role="main">
 <?=$page->body ?>
+</div>
+
 </body>
 </html>
